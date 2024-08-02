60785d93ee70e6363710c47197905c06123b8a45
awstringer1 <awstringer@gmail.com>
2024-08-02 14:16:31 -0400

    removed print statement

--- src/aghq-lbfgs-flextype.cpp
+++ src/aghq-lbfgs-flextype.cpp
@@ -191,7 +191,6 @@ public:
     int i=0,j=1;
     bool incr = false;
     for (int k=0;k<p;k++) {
-      if (d-i != 0) Rcpp::Rcout << "k + 1 = " << k + 1 << ", d - i = " << d - i << ", (k+1) % (d-i) = " << (k+1) % (d-i) << std::endl;
       if (d-i != 0) {
         if ((k+1) % (d-i) == 0) {
           // Note: (k+1) % (d-i) throws a runtime error with g++ but not clang++ if d-i == 0.
