b81376e26eef3e8a1e6e28801ab8cde676ba7e75
Alex Stringer <alex@alex-dev-2.cedar>
2024-08-02 15:54:39 +0000

    debugging

--- src/aghq-lbfgs-flextype.cpp
+++ src/aghq-lbfgs-flextype.cpp
@@ -190,6 +190,7 @@ public:
   void set_SuD(int l) {
     int i=0,j=1;
     for (int k=0;k<p;k++) {
+      Rcpp::Rcout << "k + 1 = " << k + 1 << ", d - i = " << d - i << ", (k+1) % (d-i) = " << (k+1) % (d-i) << std::endl;
       if ((k+1) % (d-i) == 0) {
         i++;
         j=i+1;
